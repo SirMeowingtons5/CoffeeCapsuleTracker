d3348634adc6338284899d087f09444143fa1d6a
SirMeowingtons5 <34872618+SirMeowingtons5@users.noreply.github.com>
2021-01-01 18:00:58 +0300

    🥳 INITIAL COMMIT 🥳

---
+++ buildSrc/src/main/java/com/sirmeowingtons5/coffeecapsuletracker/buildsrc/Dependencies.kt
@@ -0,0 +1,57 @@
+package com.sirmeowingtons5.coffeecapsuletracker.buildsrc
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha03"
+    const val junit = "junit:junit:4.13"
+
+    object Accompanist {
+        private const val version = "0.4.1"
+        const val coil = "dev.chrisbanes.accompanist:accompanist-coil:$version"
+        const val insets = "dev.chrisbanes.accompanist:accompanist-insets:$version"
+    }
+
+    object Kotlin {
+        private const val version = "1.4.21"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object Coroutines {
+        private const val version = "1.4.1"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+    }
+
+    object AndroidX {
+        const val coreKtx = "androidx.core:core-ktx:1.5.0-alpha05"
+
+        object Compose {
+            const val version = "1.0.0-alpha09"
+
+            const val foundation = "androidx.compose.foundation:foundation:$version"
+            const val layout = "androidx.compose.foundation:foundation-layout:$version"
+            const val ui = "androidx.compose.ui:ui:$version"
+            const val uiUtil = "androidx.compose.ui:ui-util:$version"
+            const val runtime = "androidx.compose.runtime:runtime:$version"
+            const val material = "androidx.compose.material:material:$version"
+            const val animation = "androidx.compose.animation:animation:$version"
+            const val tooling = "androidx.compose.ui:ui-tooling:$version"
+            const val iconsExtended = "androidx.compose.material:material-icons-extended:$version"
+        }
+
+        object Test {
+            private const val version = "1.2.0"
+            const val core = "androidx.test:core:$version"
+            const val rules = "androidx.test:rules:$version"
+
+            object Ext {
+                private const val version = "1.1.2-rc01"
+                const val junit = "androidx.test.ext:junit-ktx:$version"
+            }
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+    }
+}
